fc845c55cec23542615145ff9d5e1bfdda495605
tructran1011 <trungtruc4742@gmail.com>
2021-10-11 18:05:40 +0700

    Add Google Maps dependencies

--- app/src/main/java/com/me/themoviedb/presentation/map/MapFragment.kt
+++ app/src/main/java/com/me/themoviedb/presentation/map/MapFragment.kt
@@ -4,15 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.me.themoviedb.databinding.FragmentMapBinding
 import com.me.themoviedb.presentation.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapFragment : BaseFragment<FragmentMapBinding>() {
+class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
+
+    private var mGoogleMap: GoogleMap? = null
 
     override fun inflateView(inflater: LayoutInflater, container: ViewGroup?): FragmentMapBinding {
         return FragmentMapBinding.inflate(inflater, container, false)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        binding?.mapView?.let {
+            it.onCreate(savedInstanceState)
+            it.getMapAsync(this)
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,12 +38,48 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
         setup()
     }
 
+    override fun onStart() {
+        super.onStart()
+        binding?.mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding?.mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding?.mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding?.mapView?.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding?.mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding?.mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding?.mapView?.onLowMemory()
+    }
+
     private fun setup() {
         binding?.run {
-            btnNext.setOnClickListener {
-                goToNextScreen()
-            }
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 
     private fun goToNextScreen() {
